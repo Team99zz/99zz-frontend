a9040f82828925f2cc9c7433c4a51140dc5ece3c
daniel2231 <kangheon2231@gmail.com>
2021-11-19 20:07:54 +0900

    Auth example

---
+++ components/Auth.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleLogin = async (email) => {
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signIn({ email });
+      if (error) throw error;
+      alert("Check your email for the login link!");
+    } catch (error) {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  async function googleLogin() {
+    try {
+      setLoading(true);
+      const { user, session, error } = await supabase.auth.signIn({
+        provider: "google",
+      });
+      if (error) throw error;
+      console.log(error);
+    //   alert("Google login not working")
+    } catch (error) {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="row flex flex-center">
+      <div className="col-6 form-widget">
+        <h1 className="header">Supabase + Next.js</h1>
+        <p className="description">
+          Sign in via magic link with your email below
+        </p>
+        <div>
+          <input
+            className="inputField"
+            type="email"
+            placeholder="Your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              handleLogin(email);
+            }}
+            className="button block"
+            disabled={loading}
+          >
+            <span>{loading ? "Loading" : "Send magic link"}</span>
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              googleLogin(email);
+            }}
+            className="button block"
+            disabled={loading}
+          >
+            <span>{loading ? "Loading" : "Google Login"}</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
