1eaf71d836bec7608ef5030167b5654652896bb2
jason-choi-skku <jasonchoi3@g.skku.edu>
2021-11-28 10:17:48 +0900

    내 블로그 피드 안불러와짐

--- pages/user/[uid].js
+++ pages/user/[uid].js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { supabase } from "../../utils/supabaseClient";
 import styled from "styled-components";
+import FeedCard from "../../components/feed/feedCard";
 
 import UserProfileCard from "../../components/profile/UserProfileCard";
 import UserNavCard from "../../components/profile/UserNavCard";
@@ -50,15 +51,64 @@ const options = [
 const User = ({ session }) => {
   const [loading, setLoading] = useState(true);
   const [id, setId] = useState(null);
+  const [feed, setFeed] = useState([]);
   const [username, setUsername] = useState(null);
   const [title, setTitle] = useState(null);
   const [avatar_url, setAvatarUrl] = useState(null);
 
   useEffect(() => {
     getProfile();
+    getFeed();
   }, [session]);
 
+  async function getFeed() {
+    console.log("gf")
+
+    try {
+      setLoading(true);
+      const user = await supabase.auth.user();
+
+      let { data: posting, error } = await supabase
+        .from("posting")
+        .select("id, user, title, subtitle, created_at, thumbnail")
+        .order("created_at", { ascending: false })
+
+      posting = await Promise.all(
+        posting.map(async (p) => {
+          let { data, error } = await supabase
+            .from("user")
+            .select("title, avatar_url")
+            .eq("id", p.user)
+            .single();
+
+          return {
+            ...p,
+            ...{
+              blog_title: data.title,
+              avatar_url: data.avatar_url,
+              time_ago: moment(p.created_at).fromNow(),
+            },
+          };
+        })
+      );
+
+      if (error && status !== 406) {
+        throw error;
+      }
+      if (posting) {
+        setFeed(posting);
+        console.log(posting)
+      }
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+  
   async function getProfile() {
+    console.log("gp")
+
     try {
       setLoading(true);
       const user = supabase.auth.user();
@@ -100,6 +150,18 @@ const User = ({ session }) => {
       <MyFeedDiv>
         <CustomSelect options={options} />
       </MyFeedDiv>
+      {feed.map((posting, index) => (
+
+        <FeedCard
+          key={index} href={`/user/${posting.user}/${posting.id}`}
+          title={posting.title}
+          subtitle={posting.subtitle}
+          thumbnail={posting.thumbnail}
+          avatarUrl={posting.avatar_url}
+          blogTitle={posting.blog_title}
+        ></FeedCard>
+      ))}
+
     </AccountDiv>
   );
 };
